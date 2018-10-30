0f347954ca1c834bb72cb3f047948f09841f4492
Erik <erik@noreply@gmail.com>
2018-10-30 00:14:54 -0700

    update

--- shutdown.sh
+++ shutdown.sh
@@ -1,12 +1,54 @@
 #!/bin/bash
 
+function testUp(){
+    nc -w 1  -z $1 22
+    if [[ $? -eq 1 ]];
+            then
+                return 1
+    else
+        return 0
+    fi
+}
 
-nc -w 1  -z 67.170.205.141 22
-if [[ $? -eq 1 ]];
-    then
-        shutdown -P now
 
-else
-    backUpper.sh
-    shutodwn -P now
-fi
+
+ips=('10.0.1.10' '10.0.1.97')
+while getopts "abs" option;
+    do
+
+    case $option in
+        a) #all
+            for ip in ${ips[@]};
+            do
+                if [[ $(testUp $ip) -eq 0 ]];
+                then
+                    ssh -t erik@$ip sudo shutdown -P now
+                fi
+                shift
+            done
+            shutdown -P now
+
+        ;;
+        b)     #backup
+            if [[ $(testUp "10.0.1.10") -eq 1 ]];
+            then
+                shutdown -P now
+            else
+                backUpper.sh
+                shutdown -P now
+            fi
+        ;;
+
+        s) #servers
+            for ip in ${ips[@]};
+            do
+               ssh -t erik@$ip sudo shutdown -P now
+               shift 
+            done
+        ;;
+        *)
+            echo "Invalid Input"
+        ;;
+    esac 
+done
+
