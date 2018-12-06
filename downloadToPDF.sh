837d3d82cd6866e5b769dba616acca055604c9e7
kingcobra2468 <eriktyryshkin@gmail.com>
2018-12-05 22:29:31 -0800

    code

--- downloadToPDF.sh
+++ downloadToPDF.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
+flag=$1
+shift
+
+
 for url in "$@";
     do
     ping -qc 1 $(echo $url | tr -s '/' | cut -f2 -d '/') > /tmp/1
     if [[ $? -eq 0 ]];
         then
             title=$(echo "$url" | tr -s '/' ' ' | awk -F' ' '{print $NF}')
-            wkhtmltopdf -q $url "$PWD/$title.pdf"
+            case $flag in
+                '-s')
+                    wkhtmltopdf -q $url "/tmp/$title.pdf"
+                    ;;
+                '-d')
+                    wkhtmltopdf $url "$PWD/$title.pdf"
+                    ;;
+                *)
+                    wkhtmltopdf -q $url "/tmp/$title.pdf"
+            esac
     fi
 done
 
+if [[ $flag == '-s' ]];
+    then
+    bash sendOverBT.sh /tmp/*.pdf
+fi
 
 
--- sendOverBT.sh
+++ sendOverBT.sh
@@ -4,5 +4,6 @@ MAC=$(bt-device -l | grep OnePlus | cut -f3 -d ' ')
 MAC=${MAC#(}
 MAC=${MAC%)}
 echo $MAC
+echo "$@"
 bluetooth-sendto --device=$MAC "$@"
 exit
